0e828d8034bb80f898953ff095f3949c47006920
bkhebert <bondman2004tippy@gmail.com>
2024-07-25 18:51:41 -0500

    finished Chapter 02

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -64,7 +64,19 @@ function fizzBuzz(start, end) {
    * E:
    */
   //loop through all the numbers
-  for(i = start; i < end;)
+  for( var i = start; i <= end; i ++ ){
+    if (i === 0){
+      console.log(i);
+    } else if( i % 3 === 0 && i % 5 === 0){
+      console.log('fizzbuzz');
+    } else if ( i % 5 === 0 ){
+      console.log('buzz')
+    } else if ( i % 3 === 0 ){
+      console.log('fizz')
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
